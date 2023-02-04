03d9fcf9e36e124c3210ffb3201f3b637a678f20
leduckfs <lexuanduc@kfs.com.vn>
2023-02-04 14:53:38 +0700

    fix lỗi cors img

--- js/add-text-img.js
+++ js/add-text-img.js
@@ -26,7 +26,7 @@ function DrawPlaceholder() {
         DrawOverlay(img);
         DynamicText(img)
     };
-     img.src ='./img/bg.jpg';
+     img.src ='img/bg.jpg';
     //img.src ='https://unsplash.it/' + canvas.width + '/' + canvas.height + '/?random';
 }
 function DrawOverlay(img) {
